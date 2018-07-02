4075dbd841a26631894f57dbe708296536dee582
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-07-02 18:47:33 +0200

    added support for .mp4 files, fixed a horrible typo in the README

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # YAPS (yet another pixel sorter)
 
 ## what?
-Its just another pixel sorter, nothing super special functionality-wise. It won't
+It's just another pixel sorter, nothing super special functionality-wise. It won't
 even tell you how to use it if you pass it something like -h/--help. The only
-argument it takes is an image (.jpg, .png, .tif(f), .gif, or .bmp), and it always
-outputs to output.png. It's reasonably fast (although I think using numpy would
-be faster) and it can make some pretty pictures.
+argument it takes is an image (.jpg, .png, .tif(f), .gif, or .bmp) or a video 
+(.mp4), and it always outputs to output.png (or output.mp4). 
+
+It's reasonably fast (although I think using numpy would be faster) and it can make some pretty pictures (and videos).
+
+It sorts each "band" of color (R, G, B) in the RGB colorspace of each image, rather
+than each individual pixel, so I guess that somewhat sets it apart from other pixel
+sorters.
 
 ## why?
 Because I just came out of a project where nothing I wrote made sense even when
@@ -13,5 +18,8 @@ it worked and my comments were horrible if they were present at all.
 I wanted to write something kinda neat but very simple. Something that I could 
 both understand at first glance and write good comments for.
 
-### dependencies
-Pillow 5.2.0 (earlier versions should work too)
+
+## dependencies
+Pillow 5.2.0 - required
+
+ffmpeg - optional
--- yaps.py
+++ yaps.py
@@ -1,64 +1,129 @@
-from PIL import Image
-from sys import argv
 import os
 import random
+import shutil
+from sys import argv
+
+from PIL import Image
+
 
 IMG_FORMATS = ['.jpg', '.jpeg', '.png', '.tif', '.tiff', '.gif', '.bmp']
-
-#Read image
-if os.path.isfile(argv[1]):
-    if os.path.splitext(argv[1])[1].lower() in IMG_FORMATS:
-        im = Image.open(argv[1])
-
-im_width, im_height = im.size
+VID_FORMATS = ['.mp4']
 
 
-#Splitting the image into its respective bands, i.e. Red, Green,
-#and Blue for RGB
-r,g,b = im.split()
+def image_handler(image):
 
-#list to hold our bands when they're sorted
-new_bands = []
-
-for band in [r, g, b]:
-
-    #convert the band into a bytearray
-    band_list = list(band.tobytes())
-
-    #convert the list of all the pixels into lists of pixel in each row
-    rows = []
-    for i in range(0, len(band_list), im_width):
-        rows.append(band_list[i:i + im_width])
-
-    #sort each row
-    for each in rows:
-        each.sort()
-
-    #put our list of rows back into one big ole' list
-    new_band_list = []
-    for each in rows:
-        for element in each:
-            new_band_list.append(element)
-
-    #put our bytearray back into a bytes object
-    band_bytes = bytes(new_band_list)
-
-    #recreate the band from the mode of the original band, our bytes object,
-    #and the size of the original image
-    band = Image.frombytes(band.mode, im.size, band_bytes)
-
-    #add our newly sorted band to the list of sorted bands
-    new_bands.append(band)
+    image_new = sort_image(image)
+    image_new.save("output.png")
 
 
-#put the sorted bands of the picture back together. . . 
-im_new = Image.merge("RGB", tuple(new_bands))
+def video_handler(video):
 
-# . . . save the image. . . 
-im_new.save("output.png")
+    #make some temporary folders
+    if not os.path.exists("temp/input"):
+        os.makedirs("temp/input")
+    
+    if not os.path.exists("temp/output"):
+        os.makedirs("temp/output")
 
-#. . and show the image
-im_new.show()
+    #convert the given video to a set of frames, store them in a temp input directory
+    print("Converting video to frames. . . .")
+    os.system("ffmpeg -i {} -r 30 temp/input/frame_%05d.jpeg >> /dev/null 2>&1".format(video))
 
 
-pause = input("Hit enter to close. . . ")
+    print("Sorting frames. . . .")
+
+    #I'm going to be using these values potentially thousands of times. It looks
+    #hella amateur but it's better this way.
+    file_list = os.listdir("temp/input")
+    num_files = len(file_list)
+    i = 0
+
+    for file in file_list:
+        
+        current_progress = int((i / num_files) * 100)
+
+        #update progress every 5%
+        if current_progress % 5 == 0:
+            print(str(current_progress) + '% complete', end="\r")
+        
+        #open the jpeg of the frame, sort it, and store it as sorted_frame
+        sorted_frame = sort_image(Image.open("temp/input/" + file))
+
+        #save the sorted frame to a temporary output directory
+        sorted_frame.save("temp/output/" + file)
+
+        i += 1
+    
+    print("Converting sorted frames to video. . . .")
+    #convert the images from the temporary output directory to a video
+    os.system("ffmpeg -framerate 30 -i temp/output/frame_%05d.jpeg output.mp4 >> /dev/null 2>&1")
+
+    print("Cleaning up. . . .")
+    #finally, delete our temporary folder
+    shutil.rmtree("temp")
+
+    print("Done!")
+
+
+#Accepts an unsorted Image object, returns a sorted Image object
+def sort_image(im):
+
+    im_width, im_height = im.size
+
+
+    #Splitting the image into its respective bands, i.e. Red, Green,
+    #and Blue for RGB
+    r,g,b = im.split()
+
+    #list to hold our bands when they're sorted
+    new_bands = []
+
+    for band in [r, g, b]:
+
+        #convert the band into a bytearray
+        band_list = list(band.tobytes())
+
+        #convert the list of all the pixels into lists of pixel in each row
+        rows = []
+        for i in range(0, len(band_list), im_width):
+            rows.append(band_list[i:i + im_width])
+
+        #sort each row
+        for each in rows:
+            each.sort()
+
+        #put our list of rows back into one big ole' list
+        new_band_list = []
+        for each in rows:
+            for element in each:
+                new_band_list.append(element)
+
+        #put our bytearray back into a bytes object
+        band_bytes = bytes(new_band_list)
+
+        #recreate the band from the mode of the original band, our bytes object,
+        #and the size of the original image
+        band = Image.frombytes(band.mode, im.size, band_bytes)
+
+        #add our newly sorted band to the list of sorted bands
+        new_bands.append(band)
+
+
+    #put the sorted bands of the picture back together. . . 
+    im_new = Image.merge("RGB", tuple(new_bands))
+
+    return im_new
+
+
+def main():
+    #Read image or video
+    if os.path.isfile(argv[1]):
+
+        file_extension = os.path.splitext(argv[1])[1].lower()
+        if file_extension in IMG_FORMATS:
+            image_handler(Image.open(argv[1]))
+
+        elif file_extension in VID_FORMATS:
+            video_handler(argv[1])
+
+main()
